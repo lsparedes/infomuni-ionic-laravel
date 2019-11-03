682f9ebcfb08b457998b53f710fb32145cdf0a43
Lorenzo <lsparedes@ing.ucsc.cl>
2019-11-03 01:41:23 -0300

    Subida Inicial

---
+++ IonicPassport/src/settings/Laravel.ts
@@ -0,0 +1,18 @@
+export const Service: any = {
+
+    /**
+     * Url of your Laravel Project
+     */
+    url: 'http://127.0.0.1:8000',
+    apiUrl: 'http://127.0.0.1:8000/api',
+
+    /**
+     * Info of your passport client, usually second record on table "oauth_clients" in your database, name "Laravel Password Grant Client"
+     */
+    passport: {
+        'grant_type': 'password',
+        'client_id': '2',
+        'client_secret': 'iWWgLTgP4vD5axXtHZJ6cUyr6GSUMX9a8qDTxGj9',
+    }
+
+};
